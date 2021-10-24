8db5b4035bd555cc3ff79f9040d5a128c4d21420
di <liudi25ece@outlook.com>
2021-10-24 12:01:48 -0400

    End of section 14

---
+++ API/Data/LikeRepository.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using API.Helpers;
+using API.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class LikeRepository : ILikesRepository
+    {
+        private readonly DataContext _context;
+
+        public LikeRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task<UserLike> GetUserLike(int sourceUserId, int likeUserId)
+        {
+            return await _context.Likes.FindAsync(sourceUserId, likeUserId);
+        }
+
+        public async Task<AppUser> getUserWithLikes(int userId)
+        {
+            return await _context.Users
+                .Include(x => x.LikedUsers)
+                .FirstOrDefaultAsync(x => x.Id == userId);
+        }
+
+        public async Task<PageList<LikeDto>> GetUserLikes(LikesParams likesParams)
+        {
+            var users = _context.Users.OrderBy(u => u.Username).AsQueryable();
+            var likes = _context.Likes.AsQueryable();
+
+            if (likesParams.Predicate == "liked")
+            {
+                likes = likes.Where(like => like.SourceUserId == likesParams.UserId);
+                users = likes.Select(like => like.LikedUser);
+            }
+
+            if (likesParams.Predicate == "likedBy")
+            {
+                likes = likes.Where(like => like.LikedUserId == likesParams.UserId);
+                users = likes.Select(like => like.SourceUser);
+            }
+
+            var likedUsers = users.Select(user => new LikeDto
+            {
+                Username = user.Username,
+                KnownAs = user.KnownAs,
+                Age = user.DateOfBirth.CalculateAge(),
+                PhotoUrl = user.Photos.FirstOrDefault(p => p.IsMain).Url,
+                City = user.City,
+                Id = user.Id
+            });
+
+            return await PageList<LikeDto>.CreateAsync(likedUsers, likesParams.PageNumber, likesParams.PageSize);
+        }
+    }
+}
